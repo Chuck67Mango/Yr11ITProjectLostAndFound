1b2092b7b67003cbed763b5481e082606e354ffb
Somedude14 <aryan.nair014@gmail.com>
2025-10-08 08:51:46 +1100

    changes made

--- app/src/main/java/com/example/yr11itprojectlostandfound/MainActivity.java
+++ app/src/main/java/com/example/yr11itprojectlostandfound/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.yr11itprojectlostandfound;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btnSearch = (ImageButton) findViewById(R.id.btnSearch5);
+        ImageButton btnAdd = (ImageButton) findViewById(R.id.btnAdd5);
+        ImageButton btnNotification = (ImageButton) findViewById(R.id.btnNotification5);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), search.class);
+                startActivity(intent);
+            }
+        });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), NewLog.class);
+                startActivity(intent);
+            }
+        });
+
+        btnNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Notification.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
