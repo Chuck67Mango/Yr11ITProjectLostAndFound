3aa25a214e124e3e7ec697adccc7ab2610bd748d
Somedude14 <aryan.nair014@gmail.com>
2025-10-11 11:32:52 +1100

    changes made

--- app/src/main/java/com/example/yr11itprojectlostandfound/MainActivity.java
+++ app/src/main/java/com/example/yr11itprojectlostandfound/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.yr11itprojectlostandfound;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private void showToast(String strMsg) {
+        Toast toastMsg = Toast.makeText(this, strMsg, Toast.LENGTH_SHORT);
+        toastMsg.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
         ImageButton btnSearch = (ImageButton) findViewById(R.id.btnSearch5);
         ImageButton btnAdd = (ImageButton) findViewById(R.id.btnAdd5);
         ImageButton btnNotification = (ImageButton) findViewById(R.id.btnNotification5);
+        ImageButton btnClearHistory = (ImageButton) findViewById(R.id.btnClearHistory);
+
+        EditText edtPassword = (EditText) findViewById(R.id.edtPassword);
 
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +51,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), Notification.class);
                 startActivity(intent);
+            }
+        });
+
+        btnClearHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strPassword = edtPassword.getText().toString();
+
+                if(strPassword.equals("")){
+                    showToast();
+                }
             }
         });
 
