2edef728cc2281b874044520a8d206b1fd4b45ad
Chuck67Mango <Botte-ch@student.oxley.vic.edu.au>
2025-10-14 11:35:57 +1100

    working on design

--- app/src/main/java/com/example/yr11itprojectlostandfound/Notification.java
+++ app/src/main/java/com/example/yr11itprojectlostandfound/Notification.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -23,16 +25,16 @@ public class Notification extends AppCompatActivity {
     ArrayList<String> LostArray = new ArrayList<>();
     ArrayList <String> FoundArray = new ArrayList<>();
 
-    private void readFromLost() {
+    private void readFromCSV(String FILENAME, ArrayList Array) {
         try {
-            InputStream inputStream = openFileInput(Lost);
+            InputStream inputStream = openFileInput(FILENAME);
             if (inputStream != null) {
                 InputStreamReader isr = new InputStreamReader(inputStream);
                 BufferedReader br = new BufferedReader(isr);
                 String strLine = "";
 
                 while ((strLine = br.readLine()) != null) {
-                    LostArray.add(strLine);
+                    Array.add(strLine);
                 }
             }
             //inputStream.close();
@@ -41,27 +43,7 @@ public class Notification extends AppCompatActivity {
         } catch (IOException e) {
 
         }
-    } private void readFromFound() {
-        try {
-            InputStream inputStream = openFileInput(Found);
-            if (inputStream != null) {
-                InputStreamReader isr = new InputStreamReader(inputStream);
-                BufferedReader br = new BufferedReader(isr);
-                String strLine = "";
-
-                while ((strLine = br.readLine()) != null) {
-                    FoundArray.add(strLine);
-                }
-
-            }
-            //inputStream.close();
-        } catch (FileNotFoundException e) {
-
-        } catch (IOException e) {
-
-        }
-    }
-
+   }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,9 +54,18 @@ public class Notification extends AppCompatActivity {
         ImageButton btnAdd = (ImageButton) findViewById(R.id.btnAdd4);
         ImageButton btnNotification = (ImageButton) findViewById(R.id.btnNotification4);
         ImageButton btnSettings = (ImageButton) findViewById(R.id.btnSettings4);
+        ListView listViewNotifications = (ListView) findViewById(R.id.listViewNotifications);
 
         //make the code read file search for found items that match description of lost items then display notification
 
+        readFromCSV(Found, FoundArray);
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1,FoundArray);
+
+
+        listViewNotifications.setAdapter(adapter);
 
 
 
